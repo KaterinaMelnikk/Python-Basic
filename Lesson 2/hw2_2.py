eca584caa2e224717b70b1c75d6ba4ef659f9d20
katerinamelnikk <kate.mlnkk@gmail.com>
2024-10-24 20:51:20 +0200

    updates

---
+++ Lesson 2/hw2_2.py
@@ -0,0 +1,10 @@
+number = int(input("Введіть 5-ти значне число: "))
+
+d1 = number // 10000
+d2 = (number // 1000) % 10
+d3 = (number // 100) % 10
+d4 = (number // 10) % 10
+d5 = number % 10
+
+reverse_number= (d5 * 10000) + (d4 * 1000) + (d3 * 100) + (d2 *10) + d1
+print(reverse_number)
